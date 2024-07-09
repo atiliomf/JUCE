d591eb41eb2b8a5e609cc77d1392dbcdd16f062a
atiliomf <81830200+atiliomf@users.noreply.github.com>
2024-07-09 20:12:20 +0200

    Update juce_InAppPurchases_ios.cpp

--- modules/juce_product_unlocking/native/juce_InAppPurchases_ios.cpp
+++ modules/juce_product_unlocking/native/juce_InAppPurchases_ios.cpp
@@ -1,24 +1,33 @@
 /*
   ==============================================================================
 
-   This file is part of the JUCE library.
-   Copyright (c) 2022 - Raw Material Software Limited
+   This file is part of the JUCE framework.
+   Copyright (c) Raw Material Software Limited
 
-   JUCE is an open source library subject to commercial or open-source
+   JUCE is an open source framework subject to commercial or open source
    licensing.
 
-   By using JUCE, you agree to the terms of both the JUCE 7 End-User License
-   Agreement and JUCE Privacy Policy.
+   By downloading, installing, or using the JUCE framework, or combining the
+   JUCE framework with any other source code, object code, content or any other
+   copyrightable work, you agree to the terms of the JUCE End User Licence
+   Agreement, and all incorporated terms including the JUCE Privacy Policy and
+   the JUCE Website Terms of Service, as applicable, which will bind you. If you
+   do not agree to the terms of these agreements, we will not license the JUCE
+   framework to you, and you must discontinue the installation or download
+   process and cease use of the JUCE framework.
 
-   End User License Agreement: www.juce.com/juce-7-licence
-   Privacy Policy: www.juce.com/juce-privacy-policy
+   JUCE End User Licence Agreement: https://juce.com/legal/juce-8-licence/
+   JUCE Privacy Policy: https://juce.com/juce-privacy-policy
+   JUCE Website Terms of Service: https://juce.com/juce-website-terms-of-service/
 
-   Or: You may also use this code under the terms of the GPL v3 (see
-   www.gnu.org/licenses).
+   Or:
 
-   JUCE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
-   EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
-   DISCLAIMED.
+   You may also use this code under the terms of the AGPLv3:
+   https://www.gnu.org/licenses/agpl-3.0.en.html
+
+   THE JUCE FRAMEWORK IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL
+   WARRANTIES, WHETHER EXPRESSED OR IMPLIED, INCLUDING WARRANTY OF
+   MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, ARE DISCLAIMED.
 
   ==============================================================================
 */
@@ -45,10 +54,10 @@ struct InAppPurchases::Pimpl
 
         String getProductId()      const override  { return nsStringToJuce (download.contentIdentifier); }
         String getContentVersion() const override  { return nsStringToJuce (download.contentVersion); }
+        Status getStatus()         const override  { return SKDownloadStateToDownloadStatus (download.state); }
 
       #if JUCE_IOS
         int64 getContentLength()   const override  { return download.expectedContentLength; }
-        Status getStatus()         const override  { return SKDownloadStateToDownloadStatus (download.state); }
       #else
         int64 getContentLength()   const override
         {
@@ -57,7 +66,6 @@ struct InAppPurchases::Pimpl
 
             return download.contentLength.longLongValue;
         }
-        Status getStatus()         const override  { return SKDownloadStateToDownloadStatus (download.state); }
       #endif
 
         SKDownload* download;
@@ -791,13 +799,6 @@ private:
                     }
                 }
             });
-
-#if JUCE_IOS && defined (__IPHONE_11_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_11_0
-            addMethod (@selector (paymentQueue:shouldAddStorePayment:forProduct:), [] (id, SEL, SKPaymentQueue*, SKPayment*, SKProduct*) 
-            {
-                
-            });
-#endif
 
             registerClass();
         }

844d74635d207732cd16cda4a1308c59977dcb99
unexpected-inventions <atiliomf@gmail.com>
2026-01-31 15:07:25 +0100

    Updates to match develop branch

--- modules/juce_graphics/native/juce_Fonts_freetype.cpp
+++ modules/juce_graphics/native/juce_Fonts_freetype.cpp
@@ -394,10 +394,8 @@ public:
         if (face == nullptr)
             return {};
 
-        auto* hbFace = hb_ft_face_create_referenced (face->face);
-        const ScopeGuard scope { [&] { hb_face_destroy (hbFace); } };
-
-        HbFont hb { hb_font_create (hbFace) };
+        HbFace hbFace { hb_ft_face_create_referenced (face->face), IncrementRef::no };
+        HbFont hb { hb_font_create (hbFace.get()), IncrementRef::no };
 
         if (hb == nullptr)
             return {};
@@ -413,20 +411,13 @@ public:
         if (face == nullptr)
             return {};
 
-        auto* hbFace = hb_ft_face_create_referenced (face->face);
-        const ScopeGuard scope { [&] { hb_face_destroy (hbFace); } };
-
-        HbFont hb { hb_font_create (hbFace) };
+        HbFace hbFace { hb_ft_face_create_referenced (face->face), IncrementRef::no };
+        HbFont hb { hb_font_create (hbFace.get()), IncrementRef::no };
 
         if (hb == nullptr)
             return {};
 
         return new FreeTypeTypeface (DoCache::yes, face, std::move (hb), face->face->family_name, face->face->style_name);
-    }
-
-    Native getNativeDetails() const override
-    {
-        return Native { hb.get(), nonPortableMetrics };
     }
 
     Typeface::Ptr createSystemFallback ([[maybe_unused]] const String& text,
@@ -493,9 +484,16 @@ public:
 
     ~FreeTypeTypeface() override
     {
+        native.reset();
+
         if (doCache == DoCache::yes)
             if (auto* list = FTTypefaceList::getInstanceWithoutCreating())
                 list->removeMemoryFace (ftFace);
+    }
+
+    const Native* getNativeDetails() const override
+    {
+        return native.get();
     }
 
     static Typeface::Ptr findSystemTypeface()
@@ -545,8 +543,8 @@ private:
             if (face == nullptr)
                 return {};
 
-            const HbFace hbFace { hb_ft_face_create_referenced (face->face) };
-            HbFont cachedFont { hb_font_create (hbFace.get()) };
+            HbFace hbFace { hb_ft_face_create_referenced (face->face), IncrementRef::no };
+            HbFont cachedFont { hb_font_create (hbFace.get()), IncrementRef::no };
 
             if (cachedFont == nullptr)
                 return {};
@@ -556,6 +554,15 @@ private:
     }
    #endif
 
+    static TypefaceAscentDescent getNativeMetrics (FTFaceWrapper::Ptr ftFace)
+    {
+        const auto upem = (float) ftFace->face->units_per_EM;
+        const auto ascent = (float) std::abs (ftFace->face->ascender) / upem;
+        const auto descent = (float) std::abs (ftFace->face->descender) / upem;
+
+        return { ascent, descent };
+    }
+
     FreeTypeTypeface (DoCache cache,
                       FTFaceWrapper::Ptr ftFaceIn,
                       HbFont hbIn,
@@ -563,8 +570,8 @@ private:
                       const String& styleIn)
         : Typeface (nameIn, styleIn),
           ftFace (ftFaceIn),
-          hb (std::move (hbIn)),
-          doCache (cache)
+          doCache (cache),
+          native (std::make_unique<Native> (TypefaceNativeOptions { std::move (hbIn), getNativeMetrics (ftFaceIn) }))
     {
         if (doCache == DoCache::yes)
             if (auto* list = FTTypefaceList::getInstance())
@@ -572,10 +579,8 @@ private:
     }
 
     FTFaceWrapper::Ptr ftFace;
-    HbFont hb;
     DoCache doCache;
-    TypefaceAscentDescent nonPortableMetrics { (float) std::abs (ftFace->face->ascender)  / (float) ftFace->face->units_per_EM,
-                                               (float) std::abs (ftFace->face->descender) / (float) ftFace->face->units_per_EM };
+    std::unique_ptr<Native> native;
 
     JUCE_DECLARE_NON_COPYABLE (FreeTypeTypeface)
 };

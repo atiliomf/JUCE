248fcc2d2fab37bc5c2fe102c301e3ccee02c969
unexpected-inventions <atiliomf@gmail.com>
2026-02-06 17:46:05 +0100

    Update juce_SystemStats.cpp

--- modules/juce_core/system/juce_SystemStats.cpp
+++ modules/juce_core/system/juce_SystemStats.cpp
@@ -290,7 +290,7 @@ String SystemStats::getStackBacktrace()
         if (Dl_info info; dladdr (stack[i], &info))
         {
             int status;
-            std::unique_ptr<char, decltype (::free)*> demangled (abi::__cxa_demangle (info.dli_sname, nullptr, 0, &status), ::free);
+            std::unique_ptr<char, decltype (::free)*> demangled (abi::__cxa_demangle (info.dli_sname, nullptr, nullptr, &status), ::free);
             
             if (status == 0)
             {
